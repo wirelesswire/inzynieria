ad617bcea8bdb795538ebbf0ab763c57f201bde1
wire <brnrml007@gmail.com>
2024-11-25 17:43:39 +0100

    test2

--- biznesowa/widok.java
+++ biznesowa/widok.java
@@ -185,4 +185,13 @@ public class widok implements zmianaWidoku {
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * 
+	 * @param dane
+	 */
+	private void wynikLogowania(daneDlaUzytkownika dane) {
+		// TODO - implement widok.wynikLogowania
+		throw new UnsupportedOperationException();
+	}
+
 }
