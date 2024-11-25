4c08689ea5ff37fc1552cca823744a80c8a30107
wire <brnrml007@gmail.com>
2024-11-25 17:30:04 +0100

    initial2

---
+++ biznesowa/logowanieStrategy.java
@@ -0,0 +1,14 @@
+package biznesowa;
+
+public class logowanieStrategy implements strategia {
+
+	/**
+	 * 
+	 * @param dane
+	 */
+	public void przetworzDane(dane dane) {
+		// TODO - implement logowanieStrategy.przetworzDane
+		throw new UnsupportedOperationException();
+	}
+
+}
